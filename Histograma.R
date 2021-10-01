d988b2dc343d4533505c9cdee25c6eab81e6641a
Selene Muñoz <mu144359@uaeh.edu.mx>
2021-10-01 16:55:49 -0500

    Se anexa script, cuaderno MD, datos (input) e histogramas (output)

---
+++ Histograma.R
@@ -0,0 +1,50 @@
+####################
+#                  #
+# Copiar todo esto #
+#                  #
+####################
+# Hecho con gusto por Selene Muñoz ORtega (UAEH)
+#V4
+# LABORATORIO - Gráficos en R con ggplot2 para Ciencia de Datos
+# Histogramas en R
+
+
+
+#instalando paquete con los datos
+#install.packages("gapminder")
+#install.packages("ggplot2")
+# Histogramas en R
+
+
+# cargar libreria ggplot2 y gapminder
+library(ggplot2)
+library(gapminder)
+
+
+# cargando datos a entorno
+data("gapminder")
+
+# filtrando por año 2007
+gapminder2007 <- gapminder[gapminder$year == '2007', ]
+
+
+# hacer un histograma en ggplot2
+ggplot(data=gapminder2007,
+       mapping = aes(x=lifeExp))+
+  geom_histogram()
+  
+
+# hacer un histograma en ggplot2 con 9 intervalos (bins)
+ggplot(data=gapminder2007,
+       mapping = aes(x=lifeExp))+
+  geom_histogram(bins = 9)
+
+
+# haciendo más cosas interesantes
+ggplot(data=gapminder2007,
+       mapping = aes(x=lifeExp,
+                    fill=factor(continent)))+
+  geom_histogram(bins = 9)
+
+
+
